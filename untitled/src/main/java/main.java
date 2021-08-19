aa793d4e00d10b2a534679861852cc84e893808a
jinkaichao <jinkaichao@kuaishou.com>
2021-08-19 15:32:44 +0800

    master

---
+++ untitled/src/main/java/main.java
@@ -0,0 +1,9 @@
+/**
+ * @author jinkaichao <jinkaichao@kuaishou.com>
+ * Created on 2021-08-19
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
